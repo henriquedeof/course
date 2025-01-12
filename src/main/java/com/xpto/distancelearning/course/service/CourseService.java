f5aa736adf995e151cb2079ec650b459520c93b4
henri <henriquedeof@gmail.com>
2025-01-13 10:53:31 +1100

    Command messages (async) 1. Async communication between course and notification services.

--- src/main/java/com/xpto/distancelearning/course/service/CourseService.java
+++ src/main/java/com/xpto/distancelearning/course/service/CourseService.java
@@ -1,6 +1,7 @@
 package com.xpto.distancelearning.course.service;
 
 import com.xpto.distancelearning.course.models.CourseModel;
+import com.xpto.distancelearning.course.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -21,4 +22,6 @@ public interface CourseService {
     boolean existsByCourseAndUser(UUID courseId, UUID userId);
 
     void saveSubscriptionUserInCourse(UUID courseId, UUID userId);
+
+    void saveSubscriptionUserInCourseAndSendNotification(CourseModel course, UserModel user);
 }
